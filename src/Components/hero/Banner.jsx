520b2de5a6b155e296318f2340ce8aa4c8bcb1be
Antonela Borgogno <antonelaborgogno@gmail.com>
2022-11-05 18:35:18 -0300

    boton muted

--- src/Components/hero/Banner.jsx
+++ src/Components/hero/Banner.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Title from './Title'
 import Descripcion from './Descripcion'
 import Button from './Button'
@@ -8,21 +8,25 @@ import {faPlay} from '@fortawesome/free-solid-svg-icons'
 import {faCircleInfo} from '@fortawesome/free-solid-svg-icons'
 import Encanto from '../../video/Encanto.mp4'
 
-
 const Banner = () => {
+    const [isMuted, setIsMuted] = useState(true)
 
     return (
         <div className={style.banner}>
-            <video className={style.video} autoPlay loop muted>
+            <video className={style.video} autoPlay loop muted={isMuted}>
                 <source src={Encanto} type="video/mp4"/>
             </video>
+
             <Title titulo='Encanto'/>
+
             <Descripcion texto='Cuenta la historia de una familia extraordinaria, los Madrigal, que viven escondidos en las montañas de Colombia, en una casa mágica situada en un enclave maravilloso llamado Encanto. La magia de Encanto ha dotado a todos los niños de la familia un don único, desde la súper fuerza hasta el poder de curar... Pero se olvidó de un miembro de la familia: Mirabel. Cuando ésta descubre que la magia que rodea Encanto está en peligro, decide que ella, la única Madrigal normal, podría ser la última esperanza de su extraordinaria familia.'/>
+
             <div className={style.botones}>
                 <Button icono={faPlay} color='black' background="#FFFFFF60" contenido='Reproducir'/>
                 <Button icono={faCircleInfo} color='white' background="#45454590" contenido ='Más información'/>
             </div>
-            <Clasificacion edad='13'/>
+            
+            <Clasificacion edad='13' setIsMuted={setIsMuted} isMuted={isMuted}/>
         </div>
     );
 };
--- src/Components/hero/Clasificacion.jsx
+++ src/Components/hero/Clasificacion.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import style from './Clasificacion.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faVolumeMute} from '@fortawesome/free-solid-svg-icons'
+import {faVolumeMute, faVolumeOff} from '@fortawesome/free-solid-svg-icons'
 
-const Clasificacion = ({edad}) => {
+const Clasificacion = ({edad, setIsMuted, isMuted}) => {
     return (
         <div className={style.contenedor}>
-            <FontAwesomeIcon style={{ width:"25px", height:"25px",color:"white", marginRight: "10px"}} icon={faVolumeMute}/>
+            {
+                !isMuted ?
+                <FontAwesomeIcon style={{ width:"25px", height:"25px",color:"white", marginRight: "10px"}} icon={faVolumeOff} onClick={(e) => setIsMuted(true)}/>
+                :
+                <FontAwesomeIcon style={{ width:"25px", height:"25px",color:"white", marginRight: "10px"}} icon={faVolumeMute} onClick={(e) => setIsMuted(false)}/>
+            }
+
             <div className={style.clasificacion}>
                 <h3>{edad} +</h3>
             </div>
